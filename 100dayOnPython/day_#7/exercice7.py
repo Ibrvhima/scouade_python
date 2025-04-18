6c2ee55ef5a8833a25693c868ba7277019bd7d9a
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-04-18 22:27:04 +0000

    exercice7 day7

--- 100dayOnPython/day_#7/exercice7.py
+++ 100dayOnPython/day_#7/exercice7.py
@@ -1,8 +1,9 @@
 nbr = int(input("Donner un nombre : "))
 somme = 0
 
-for i in range(1, nbr + 1):
-     somme += i
-
-print(f"La somme des entiers de 1 à {nbr} = {somme}")
+while nbr > 0:
+     reste = nbr % 10
+     somme += reste
+     nbr //= 10
+print(f"la somme est {somme}")
 
