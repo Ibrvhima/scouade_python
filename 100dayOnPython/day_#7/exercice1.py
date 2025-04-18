e6570a568eca4d6e1c5d36b1174f87f1138cb409
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-04-18 22:26:45 +0000

    exercices 1 day 7

--- 100dayOnPython/day_#7/exercice1.py
+++ 100dayOnPython/day_#7/exercice1.py
@@ -2,4 +2,4 @@ number = int(input("Donner un nombre "))
 if number % 2 == 0:
      print(f'{number} est un nombre Paire')
 else:
-     print(f'{number} est un nombre Paire')
+     print(f'{number} est un nombre Impaire')
